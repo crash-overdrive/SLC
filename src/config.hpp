bdda0a4ebf5ce350e6fe1bf1bcb19b8063d12561
cgwelcome <charlesgao.c@gmail.com>
2020-01-31 20:29:01 -0500

    Add environment configuration header

---
+++ src/config.hpp
@@ -0,0 +1,8 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include <string>
+
+const std::string library_directory = "/home/charles/Projects/cs444-w20-group20/build/lib";
+
+#endif // CONFIG_HPP
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,5 +1,11 @@
+#include <iostream>
+#include <fstream>
 #include "parser.hpp"
+#include "config.hpp"
 
 bool Parser::done() {
+	std::ifstream file;
+	file.open(library_directory + "/grammar/sample.cfg");
+	file.close();
 	return false;
 }
