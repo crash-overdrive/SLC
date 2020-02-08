16c15f848b5f7d54ba9860a63d447ddf22d3a0ae
cgwelcome <charlesgao.c@gmail.com>
2020-02-08 18:21:58 -0500

    Fix compiling issues

--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -5,6 +5,8 @@
 
 constexpr int EPSILON = -1;
 
+namespace Lex {
+
 class NfaTransition {
   public:
   int previousState;
@@ -54,16 +56,16 @@ class Nfa {
   }
 
   void shiftStates(int shiftValue) {
-    for (int i = 0; i < states.size(); ++i) {
+    for (size_t i = 0; i < states.size(); ++i) {
       states[i] += shiftValue;
     }
 
-    for (int i = 0; i < transitions.size(); ++i) {
+    for (size_t i = 0; i < transitions.size(); ++i) {
       transitions[i].previousState += shiftValue;
       transitions[i].nextState += shiftValue;
     }
 
-    for(int i = 0; i < acceptingStates.size(); ++i) {
+    for(size_t i = 0; i < acceptingStates.size(); ++i) {
       acceptingStates[i] += shiftValue;
     }
 
@@ -252,9 +254,6 @@ class DFA {
   std::vector<int> acceptingStates;
 };
 
-DFA nfaToDfaConversion(Nfa nfa) {
-
-}
 
 class Definition {
   public:
@@ -263,59 +262,61 @@ class Definition {
   Definition(std::string type, std::string regex): type(type), regex(regex) {};
 };
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cout << "Invalid number of arguments passed, required: 2, got: " << argc << std::endl;
-    return 42;
-  }
+} // Lex
 
-  std::ifstream file;
-  file.open(argv[1]);
+//int main(int argc, char *argv[]) {
+  //if (argc != 2) {
+    //std::cout << "Invalid number of arguments passed, required: 2, got: " << argc << std::endl;
+    //return 42;
+  //}
 
-  std::vector<Definition> definitions;
-  std::string delimiter = " -=- ";
+  //std::ifstream file;
+  //file.open(argv[1]);
 
-  if (file.is_open()) {
-      while (!file.eof()) {
-        std::string line;
+  //std::vector<Definition> definitions;
+  //std::string delimiter = " -=- ";
 
-        std::getline(file, line);
-        // std::cout << "Read: " << line << std::endl;
-        int position = line.find(delimiter);
-        definitions.push_back(Definition(line.substr(0, position), line.substr(position + delimiter.length())));
-      }
-      file.close();
-  } else {
-    std::cout << "File: " << argv[1] << " not found.. quitting" << std::endl;
-    return 42;
-  }
+  //if (file.is_open()) {
+      //while (!file.eof()) {
+        //std::string line;
 
-  std::cout << "Definitions generated: " << std::endl;
-  for(auto const& definition: definitions) {
-   std::cout << "[" << definition.type << "] {" << definition.regex << "}" << std::endl;
-  }
+        //std::getline(file, line);
+        //// std::cout << "Read: " << line << std::endl;
+        //int position = line.find(delimiter);
+        //definitions.push_back(Definition(line.substr(0, position), line.substr(position + delimiter.length())));
+      //}
+      //file.close();
+  //} else {
+    //std::cout << "File: " << argv[1] << " not found.. quitting" << std::endl;
+    //return 42;
+  //}
 
-  Nfa nfa;
-  nfa.initialiseStates(4);
-  nfa.addTransition(1, 2, 0);
-  nfa.addTransition(1, 3, EPSILON);
-  nfa.addTransition(2, 2, 1);
-  nfa.addTransition(2, 4, 1);
-  nfa.addTransition(3, 2, EPSILON);
-  nfa.addTransition(3, 4, 0);
-  nfa.addTransition(4, 3, 0);
-  nfa.setStartState(1);
-  nfa.setAcceptingStates({3,4});
-  std::vector<int> test1 = nfa.epsilonClosure({1});
-  for (auto const& num: test1) {
-    std::cout << num << " ";
-  }
+  //std::cout << "Definitions generated: " << std::endl;
+  //for(auto const& definition: definitions) {
+   //std::cout << "[" << definition.type << "] {" << definition.regex << "}" << std::endl;
+  //}
 
-  std::cout << std::endl;
+  //Nfa nfa;
+  //nfa.initialiseStates(4);
+  //nfa.addTransition(1, 2, 0);
+  //nfa.addTransition(1, 3, EPSILON);
+  //nfa.addTransition(2, 2, 1);
+  //nfa.addTransition(2, 4, 1);
+  //nfa.addTransition(3, 2, EPSILON);
+  //nfa.addTransition(3, 4, 0);
+  //nfa.addTransition(4, 3, 0);
+  //nfa.setStartState(1);
+  //nfa.setAcceptingStates({3,4});
+  //std::vector<int> test1 = nfa.epsilonClosure({1});
+  //for (auto const& num: test1) {
+    //std::cout << num << " ";
+  //}
 
-  std::vector<int> test2 = nfa.epsilonClosure({2,3});
-  for (auto const& num: test2) {
-    std::cout << num << " ";
-  }
-  return 0;
-}
+  //std::cout << std::endl;
+
+  //std::vector<int> test2 = nfa.epsilonClosure({2,3});
+  //for (auto const& num: test2) {
+    //std::cout << num << " ";
+  //}
+  //return 0;
+//}
