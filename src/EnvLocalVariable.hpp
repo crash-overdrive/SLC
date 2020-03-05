bb7374d1e7f12ab1c33eea48874cad862747eb4d
cgwelcome <charlesgao.c@gmail.com>
2020-03-04 23:07:04 -0500

    Add EnvLocalVariable Scope

---
+++ src/EnvLocalVariable.hpp
@@ -0,0 +1,21 @@
+#include <memory>
+#include <unordered_set>
+#include <vector>
+
+namespace Env {
+
+class LocalVariableScope {
+public:
+  LocalVariableScope(LocalVariableScope *Parent = nullptr);
+  LocalVariableScope *pop() const;
+  LocalVariableScope *push();
+  bool lookUp(const std::string &Name);
+  bool add(const std::string &Name);
+
+private:
+  LocalVariableScope *Parent;
+  std::unordered_set<std::string> Variables{};
+  std::vector<std::unique_ptr<LocalVariableScope>> Children{};
+};
+
+}; // namespace Env
