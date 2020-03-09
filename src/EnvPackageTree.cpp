45918b410965e6901c3f21ae9ab80893185cb17c
cgwelcome <charlesgao.c@gmail.com>
2020-03-08 23:15:27 -0400

    Rename scope to EnvPackage

---
+++ src/EnvPackageTree.cpp
@@ -0,0 +1,67 @@
+#include "EnvPackageTree.hpp"
+
+namespace Env {
+
+PackageNode::PackageNode(Type type, const std::string &name)
+    : type(type), name(name) {}
+
+PackageNode *PackageNode::update(Type type, const std::string &name) {
+  if (this->type != GLOBAL && this->type != PACKAGE)
+    return nullptr;
+  switch (type) {
+  case PACKAGE:
+    return updatePackage(type, name);
+    break;
+  case CLASS:
+  case INTERFACE:
+    return addType(type, name);
+    break;
+  case GLOBAL:
+    break;
+  }
+  return nullptr;
+}
+
+PackageNode *PackageNode::find(const std::string &name) {
+  auto it = children.find(name);
+  return (it != children.end()) ? &it->second : nullptr;
+}
+
+PackageNode *PackageNode::updatePackage(Type type, const std::string &name) {
+  auto It = children.find(name);
+  if (It != children.end()) {
+    return (It->second.type == PACKAGE) ? &It->second : nullptr;
+  }
+  auto ChildIt = children.emplace(name, PackageNode{type, name});
+  return &ChildIt.first->second;
+}
+
+PackageNode *PackageNode::addType(Type type, const std::string &name) {
+  auto It = children.emplace(name, PackageNode{type, name});
+  return It.second ? &It.first->second : nullptr;
+}
+
+void PackageTreeVisitor::visit(const AST::PackageDeclaration &Decl) {
+  type = PackageNode::PACKAGE;
+  dispatchChildren(Decl);
+}
+
+void PackageTreeVisitor::visit(const AST::ClassDeclaration &Decl) {
+  if (type == PackageNode::GLOBAL)
+    return;
+  type = PackageNode::CLASS;
+  dispatchChildren(Decl);
+}
+
+void PackageTreeVisitor::visit(const AST::InterfaceDeclaration &Decl) {
+  if (type == PackageNode::GLOBAL)
+    return;
+  type = PackageNode::INTERFACE;
+  dispatchChildren(Decl);
+}
+
+void PackageTreeVisitor::visit(const AST::Identifier &Identifier) {
+  (void)Identifier;
+}
+
+} // namespace Env
