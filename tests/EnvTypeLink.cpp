ac086472e2e5b61cf0e2c72b79e5b255b9c0ab43
cgwelcome <charlesgao.c@gmail.com>
2020-03-12 01:21:21 -0400

    Add test EnvTypeLink

--- tests/EnvTypeLink.cpp
+++ tests/EnvTypeLink.cpp
@@ -1,7 +1,60 @@
 #include "EnvTypeLink.hpp"
+#include "EnvFileHeader.hpp"
 #include "TestConfig.hpp"
 #include "TestUtil.hpp"
 #include "catch.hpp"
+
+TEST_CASE("EnvTypeLink", "[EnvTypeLink") {
+  Env::FileHeader mainHeader({}, {Env::Type::Class, "Main"});
+  Env::FileHeader listHeader({}, {Env::Type::Class, "List"});
+  Env::PackageTree tree;
+
+  SECTION("Simple Lookup") {
+    tree.update({"foo", "bar"}, listHeader);
+    Env::TypeLink typeLink(mainHeader, tree);
+    REQUIRE(typeLink.find({"foo", "bar", "List"}) == &listHeader);
+    REQUIRE(typeLink.find({"nonexist", "class"}) == nullptr);
+    REQUIRE(typeLink.addSingleImport({"foo", "bar", "List"}));
+    REQUIRE(typeLink.find({"List"}) == &listHeader);
+  }
+
+  SECTION("Self Lookup") {
+    Env::TypeLink typeLink(listHeader, tree);
+    REQUIRE(typeLink.find({"List"}) == &listHeader);
+  }
+
+  SECTION("No Single type import equal class same file") {
+    Env::TypeLink typeLink(listHeader, tree);
+    REQUIRE_FALSE(typeLink.addSingleImport({"foo", "bar", "List"}));
+  }
+
+  SECTION("No Single type import equal class same file") {
+    Env::TypeLink typeLink(listHeader, tree);
+    REQUIRE_FALSE(typeLink.addSingleImport({"foo", "bar", "List"}));
+  }
+
+  SECTION("Single import clash") {
+    tree.update({"foo", "canary"}, listHeader);
+    Env::TypeLink typeLink(mainHeader, tree);
+    typeLink.addSingleImport({"foo", "bar", "List"});
+    REQUIRE_FALSE(typeLink.addSingleImport({"foo", "canary", "List"}));
+  }
+
+  SECTION("Same Package Import") {
+    Env::FileHeader arrayHeader({}, {Env::Type::Class, "Array"});
+    tree.update({"foo", "canary"}, listHeader);
+    tree.update({"foo", "canary"}, arrayHeader);
+    Env::TypeLink typeLink(listHeader, tree);
+    REQUIRE(typeLink.find({"Array"}));
+  }
+
+  SECTION("On-demand clash") {
+    tree.update({"foo", "canary"}, listHeader);
+    Env::TypeLink typeLink(mainHeader, tree);
+    REQUIRE(typeLink.addDemandImport({"foo"}));
+    REQUIRE_FALSE(typeLink.find({"List"}) == nullptr);
+  }
+}
 
 TEST_CASE("EnvTypeLink Visitor", "[EnvTypeLinkVisitor]") {
   Client Client = createClient();
