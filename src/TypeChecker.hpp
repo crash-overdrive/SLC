4d43de43c803ebd6551b52e18b58c6dc663819f6
cgwelcome <charlesgao.c@gmail.com>
2020-04-03 19:56:35 -0400

    Add TypeChecking

--- src/TypeChecker.hpp
+++ src/TypeChecker.hpp
@@ -14,6 +14,14 @@ const std::unordered_set<Env::TypeKeyword> numKeyword{
     Env::TypeKeyword::Byte,
 };
 
+const std::vector<std::array<Env::Type, 2>> primitiveAssignment{
+    {Env::TypeKeyword::Simple, Env::TypeKeyword::Null},
+    {Env::TypeKeyword::Integer, Env::TypeKeyword::Short},
+    {Env::TypeKeyword::Integer, Env::TypeKeyword::Byte},
+    {Env::TypeKeyword::Integer, Env::TypeKeyword::Character},
+    {Env::TypeKeyword::Short, Env::TypeKeyword::Byte},
+};
+
 enum class BinaryOperator {
   Addition,
   Substraction,
@@ -21,13 +29,37 @@ enum class BinaryOperator {
   Division,
   Modulus,
   Equal,
+  NotEqual,
   LesserOrEqual,
   GreaterOrEqual,
   Lesser,
   Greater,
-  NotEqual,
   Or,
   And,
+};
+
+const std::unordered_set<Type::BinaryOperator> numBinaryOperator{
+    Type::BinaryOperator::Addition,       Type::BinaryOperator::Substraction,
+    Type::BinaryOperator::Multiplication, Type::BinaryOperator::Division,
+    Type::BinaryOperator::Modulus,
+};
+
+const std::unordered_set<Type::BinaryOperator> boolBinaryOperator{
+    Type::BinaryOperator::Equal,
+    Type::BinaryOperator::NotEqual,
+    Type::BinaryOperator::Or,
+    Type::BinaryOperator::And,
+};
+
+const std::unordered_set<Type::BinaryOperator> compBinaryOperator{
+    Type::BinaryOperator::Equal,
+    Type::BinaryOperator::NotEqual,
+};
+
+const std::unordered_set<Type::BinaryOperator> latticeBinaryOperator{
+    Type::BinaryOperator::Equal,          Type::BinaryOperator::LesserOrEqual,
+    Type::BinaryOperator::GreaterOrEqual, Type::BinaryOperator::Lesser,
+    Type::BinaryOperator::Greater,        Type::BinaryOperator::NotEqual,
 };
 
 struct BinaryOperation {
@@ -36,18 +68,39 @@ struct BinaryOperation {
   Env::Type ropt;
 };
 
+enum class UnaryOperator {
+  Negative,
+  Not,
+};
+
 struct UnaryOperation {
-  BinaryOperator binaryOperator;
+  UnaryOperator unaryOperator;
   Env::Type opt;
 };
 
-std::optional<Env::Type> checkBinaryOperation(const BinaryOperation &operation);
-std::optional<Env::Type> checkUnaryOperation(const UnaryOperation &operation);
-std::optional<Env::Type> checkCasting(Env::Type lopt, Env::Type ropt);
-std::optional<Env::Type> checkInstanceOf(Env::Type lopt, Env::Type ropt);
-std::optional<Env::Type> checkArrayIndex(Env::Type lopt, Env::Type ropt);
-std::optional<Env::Type> checkAssignment(Env::Type lopt, Env::Type ropt);
-bool isNum(const Env::Type &type);
+class Checker {
+public:
+  Checker(const Env::TypeLink &typeLink);
+  std::optional<Env::Type> checkAssignment(Env::Type lopt,
+                                           Env::Type ropt) const;
+  std::optional<Env::Type>
+  checkBinaryOperation(const BinaryOperation &operation) const;
+  std::optional<Env::Type>
+  checkUnaryOperation(const UnaryOperation &operation) const;
+  std::optional<Env::Type> checkCasting(Env::Type lopt, Env::Type ropt) const;
+  std::optional<Env::Type> checkInstanceOf(Env::Type lopt,
+                                           Env::Type ropt) const;
+  std::optional<Env::Type> checkArrayIndex(Env::Type lopt,
+                                           Env::Type ropt) const;
+
+private:
+  std::optional<Env::Type> checkArrayAssignment(Env::Type lopt,
+                                                Env::Type ropt) const;
+  bool isStringAddition(Env::Type lopt, Env::Type ropt) const;
+  bool isNum(Env::Type type) const;
+  bool isAssignable(Env::Type lopt, Env::Type ropt) const;
+  const Env::TypeLink &typeLink;
+};
 
 } // namespace Type
 
